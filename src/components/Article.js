487fa7516f3a9d33f0b1ff41a2f202ee12754bdb
Patrick Roberts <patrob22@gmail.com>
2023-04-10 18:08:25 -0400

    Fourth Deliverable

---
+++ src/components/Article.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Article( {
+    title, 
+    date = "January 1st, 1970", 
+    preview
+} ) {
+  return (
+    <div>
+        <h3>{title}</h3>
+        <small>{date}</small>
+        <p>{preview}</p>
+    </div>
+  )
+}
+
+export default Article
