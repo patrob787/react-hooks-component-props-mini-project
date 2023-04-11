064b967a9b858533eb33dd24ba97faa8bf8d71d6
Patrick Roberts <patrob22@gmail.com>
2023-04-10 20:18:13 -0400

    full lab complete

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,12 +3,30 @@ import React from 'react'
 function Article( {
     title, 
     date = "January 1st, 1970", 
-    preview
+    preview,
+    minutes,
 } ) {
+
+    let emojiCountFunc = (min) => {
+        let string = "";
+        if (min < 30) {
+            for (let i = 0; i < min; i+=5) {
+                string = string + "☕️"
+            }
+        } else {
+            for (let i = 30; i < min; i+=5) {
+                string = string + "🍱"
+            }
+        }
+        return string;
+    }
+    let emojiCount = emojiCountFunc(minutes);
+    console.log(emojiCount)
+
   return (
     <div>
         <h3>{title}</h3>
-        <small>{date}</small>
+        <small>{date} - {emojiCount} {minutes} minute read.</small>
         <p>{preview}</p>
     </div>
   )
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -4,7 +4,7 @@ import Article from './Article'
 function ArticleList( {posts} ) {
   
    let articleArray = posts.map((post) => {
-    return <Article key={post.id} title={post.title} date={post.date} preview={post.preview} />
+    return <Article key={post.id} title={post.title} date={post.date} preview={post.preview} minutes={post.minutes} />
    })
   
     return (
